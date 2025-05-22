a17714712c05ee3b95f4346eb6b868ea18c58d4b
theholyblack <59666976+theholyblack@users.noreply.github.com>
2025-05-22 10:29:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,20 +5,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Doomsday Clock API (Powered by Countdoom)"
+    return "Doomsday Clock API (Powered by CountdoomClient)"
 
 @app.route("/api/doomsday")
 def get_doomsday():
     try:
         client = CountdoomClient()
-countdown = client.get_clock()
-
+        countdown = client.get_clock()
         return jsonify({
             "sentence": countdown.sentence,
             "seconds": countdown.seconds,
             "clock": countdown.clock,
             "time": countdown.time,
-            "minutes": countdown.minutes,
+            "minutes": countdown.minutes
         })
     except Exception as e:
         return jsonify({"error": "Failed to get countdown", "details": str(e)}), 500
